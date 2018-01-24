248d507915a5a5b177b56a3efbbade1c8beaee0f
Michael Vezzani <mjvezzani@gmail.com>
2018-01-23 23:45:37 -0500

    Display rudimentary modal

--- src/components/info_cards.vue
+++ src/components/info_cards.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <ul class='pure-g neg-y'>
+   <ul class='pure-g neg-y'>
       <li v-for="card in cards" class='pure-u-md-1-3 pure-u-sm-1-2 pure-u-1-1'>
-        <SingleCard card=card></SingleCard>
+        <SingleCard :markup=card></SingleCard>
       </li>
     </ul>
   </div>
@@ -18,20 +18,16 @@
     data() {
       return {
         cards: [
-          { html: '<h3>Buy Me!!</h3><p>Some lorem ipsum</p><button class="pure-button button-xsmall button-dark-blue">Subscribe!</button>' },
-          { html: '<h3>Buy That!!</h3><p>More lorem ipsum</p><button class="pure-button button-xsmall button-dark-blue">Purchase!</button>' },
-          { html: '<h3>Over Here!!</h3><p>A new Car!!</p><button class="pure-button button-xsmall button-light-blue">Click for chance to win!</button>' },
-          { html: '<h3>Avoid Me</h3><p>I\'m a virus</p><button class="button-xsmall pure-button button-dark-blue">Click in Emergency Only</button>' },
-          { html: '<h3>Over Here!!</h3><p>A new Car!!</p><button class="pure-button button-xsmall button-light-blue">Click for chance to win!</button>' },
-          { html: '<h3>Buy Me!!</h3><p>Some lorem ipsum</p><button class="pure-button button-xsmall button-dark-blue">Subscribe!</button>' },
-          { html: '<h3>Avoid Me</h3><p>I\'m a virus</p><button class="button-xsmall pure-button button-dark-blue">Click in Emergency Only</button>' },
-          { html: '<h3>Buy That!!</h3><p>More lorem ipsum</p><button class="pure-button button-xsmall button-dark-blue">Purchase!</button>' },
-          { html: '<h3>Over Here!!</h3><p>A new Car!!</p><button class="pure-button button-xsmall button-light-blue">Click for chance to win!</button>' },
-          { html: '<h3>Avoid Me</h3><p>I\'m a virus</p><button class="button-xsmall pure-button button-dark-blue">Click in Emergency Only</button>' },
-          { html: '<h3>Over Here!!</h3><p>A new Car!!</p><button class="pure-button button-xsmall button-light-blue">Click for chance to win!</button>' },
-          { html: '<h3>Buy Me!!</h3><p>Some lorem ipsum</p><button class="pure-button button-xsmall button-dark-blue">Subscribe!</button>' },
+          { html: `<h3>Buy Me!!</h3>
+                   <p>Some lorem ipsum</p>
+                   <button id="show-modal" @click="showModal = true"
+                           class="pure-button button-xsmall button-dark-blue">
+                     Subscribe!
+                   </button>
+                   <modal v-if="showModal" @close="showModal = false"></modal>`,
+            title: 'Buy Me!!',
+            description: 'Some Lorem Ipsum' },
         ],
-        showModal: false,
       };
     },
   };
--- src/components/single_card.vue
+++ src/components/single_card.vue
@@ -1,11 +1,60 @@
 <template>
-  <div class='card text-center' v-html="card.html">
+  <div>
+    <script type="text/x-template" id="modal-template">
+      <transition name="modal">
+        <div class="modal-mask">
+          <div class="modal-wrapper">
+            <div class="modal-container">
+
+              <div class="modal-header">
+                <slot name="header">
+                  default header
+                </slot>
+              </div>
+
+              <div class="modal-body">
+                <slot name="body">
+                  default body
+                </slot>
+              </div>
+
+              <div class="modal-footer">
+                <slot name="footer">
+                  default footer
+                  <button class="modal-default-button" @click="$emit('close')">
+                    OK
+                  </button>
+                </slot>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </script>
+    <div class='card text-center'>
+      <h3>{{ markup.title }}</h3>
+      <p>{{ markup.description }}</p>
+      <button id="show-modal"
+              @click="showModal = true"
+              class="pure-button button-xsmall button-dark-blue">Click here</button>
+    </div>
+    <modal v-if="showModal" @close="showModal = false"></modal>
   </div>
 </template>
 
 <script>
+  import Modal from './modal';
+
   export default {
-    props: ['card'],
+    props: ['markup'],
+    components: {
+      Modal,
+    },
+    data() {
+      return {
+        showModal: false,
+      };
+    },
   };
 </script>
 
