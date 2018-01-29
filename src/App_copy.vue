5ef0de5850434a8d265e2359fa5f2b4f1a281123
Michael Vezzani <mjvezzani@gmail.com>
2018-01-29 15:34:55 -0500

    Get initial routing working

--- src/App_copy.vue
+++ src/App_copy.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <Header/>
+    <WelcomeBanner/>
+    <InfoCards/>
   </div>
 </template>
 
 <script>
+  import Header from './components/header';
+  import WelcomeBanner from './components/welcome_banner';
+  import InfoCards from './components/info_cards';
+
   export default {
     name: 'app',
+    components: {
+      Header,
+      WelcomeBanner,
+      InfoCards,
+    },
   };
 </script>
 
---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Welcome to the home page</h1>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+  };
+</script>
